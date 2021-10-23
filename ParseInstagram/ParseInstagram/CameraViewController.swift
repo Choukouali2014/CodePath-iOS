e5d66c2614706c0fa44b3eea5253950f4cf3d7eb
Frank Choukouali <fchokouali2014@gmail.com>
2021-10-22 20:18:19 -0400

    Part 2

--- ParseInstagram/ParseInstagram/CameraViewController.swift
+++ ParseInstagram/ParseInstagram/CameraViewController.swift
@@ -18,24 +18,12 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
 
         // Do any additional setup after loading the view.
     }
-    @IBAction func onCameraButton(_ sender: Any) {
-        let picker = UIImagePickerController()
-        picker.delegate = self
-        picker.allowsEditing = true
-        
-        if UIImagePickerController.isSourceTypeAvailable(.camera){
-            picker.sourceType = .camera
-        }else{
-            picker.sourceType = .photoLibrary
-        }
-        present(picker, animated: true, completion: nil)
-        
-    }
+   
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let image = info[.editedImage] as! UIImage
         let size = CGSize(width: 300, height: 300)
-        let scaledImage = image.af_imageScaled(to: size)
+        let scaledImage = image.af_imageAspectScaled(toFill: size)
         imageView.image = scaledImage
         dismiss(animated: true, completion: nil)
     }
